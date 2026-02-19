84f26faca88e5e8f9777715a9656528065f89b1f
finallydevil <mohammedn4063@masseyhigh.school.nz>
2026-02-20 12:51:26 +1300

    working loop needs headings

---
+++ B_01_rps_game_v1.py
@@ -0,0 +1,89 @@
+# checks for int more than 0 (allows <enter>)
+def int_check(question):
+    while True:
+        error = "Please enter an integer more than 1."
+
+        to_check = input(question)
+
+        # check for inf mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+
+            # check if response more than 1
+            if response < 1:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine Starts Here
+
+# initialize game variables
+mode = "regular"
+rounds_played = 0
+
+
+print("💎📃✂️ Rock / Paper / Scissors Game ️✂️📃💎")
+print()
+
+# instructions
+
+# ask user for number of rounds / infinite mode
+num_rounds = int_check("How many rounds? Or, push <enter> for infinite mode: ")
+
+if num_rounds == "infinite":
+    mode = "infinite"
+    print("You chose to play infinite mode.")
+    num_rounds = 5
+
+# game loop starts here
+while rounds_played < num_rounds:
+    user_choice = input("Choose: ")
+
+    if user_choice == "xxx":
+        break
+
+    rounds_played += 1
+    print("rounds played: ", rounds_played)
+
+    # if users in infinite mode, increase number of rounds!
+    if mode == "infinite":
+        num_rounds += 1
+
+    print("num rounds: ", num_rounds)
+
+
+# game loop ends here
+
+# game history / stats area
+
+# auto testing
+to_test = [
+    ('xlii', 'invalid'),
+    ('0.5', 'invalid'),
+    ('0', 'invalid'),
+    (1, 1),
+    (2, 2),
+    ('', 'infinite'),
+]
+
+# run test
+for i in to_test:
+    # retrieve test case
+    case = i[0]
+    expected = i[1]
+
+    #  get actual value
+    actual = int_check(case)
+
+    # comparing
+    if actual == expected:
+        print(f"✅✅✅ Passed! Case: {case}, expected: {expected}, received: {actual} ✅✅✅")
+    else:
+        print(f"❌❌❌ Failed! Case: {case}, expected: {expected}, received: {actual} ❌❌❌")
